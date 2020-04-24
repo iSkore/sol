0d2ee4e8950ec66871f72e09bbef1211ead2e37f
Nick Soggin <nicksoggin@gmail.com>
2020-04-23 21:08:52 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<v-app>
+		<Sidebar/>
+		<Toolbar/>
+
+		<v-content>
+			<v-container fluid ma-0 pa-0>
+				<router-view></router-view>
+			</v-container>
+		</v-content>
+
+		<Footer/>
+	</v-app>
+</template>
+
+<script>
+	import { mapActions } from 'vuex';
+
+	import Sidebar from '@/components/Sidebar';
+	import Toolbar from '@/components/Toolbar';
+	import Footer  from '@/components/Footer';
+
+	export default {
+		name: 'App',
+		components: {
+			Sidebar,
+			Toolbar,
+			Footer
+		},
+		data() {
+			return {};
+		},
+		beforeCreate() {
+			this.$installAxios();
+			this.$installLogger();
+		},
+		async mounted() {
+			await this.getConfig();
+		},
+		methods: {
+			...mapActions( [ 'getConfig' ] )
+		}
+	};
+</script>
+
+<style>
+	html, body {
+		overflow: hidden;
+	}
+</style>
